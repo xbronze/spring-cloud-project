043cc7a2a206b23ab31ca01aba7bd6e65de78e3c
xbronze <zihui_xu@126.com>
2025-03-05 15:52:57 +0800

    增加gateway网关的简单实现

---
+++ service-gateway/src/main/java/com/xinshi/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.xinshi.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author: xbronze
+ * @date: 2025-03-05 14:30
+ * @description: TODO
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
